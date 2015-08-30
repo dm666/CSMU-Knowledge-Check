d65a54babece2fae46565c1d6523b1a945f0ae82
dm666 <medfac666@mail.com>
2015-08-30 20:31:00 +0300

    Новые формы: список вопросов, регистрация пользователя, вывод результата на экран.

---
+++ CSMU Knowledge Check/CSMU Knowledge Check/QuestList.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CSMU_Knowledge_Check
+{
+    /// <summary>
+    /// Логика взаимодействия для QuestList.xaml
+    /// </summary>
+    public partial class QuestList : Elysium.Controls.Window
+    {
+        public QuestList()
+        {
+            InitializeComponent();
+        }
+    }
+}
